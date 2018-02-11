c87cb3bd52ae63871ce69920dfbda7585c1729f8
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-02-11 14:41:23 +0530

    buy sell working perfectly

--- newContracts/LockBuySell.sol
+++ newContracts/LockBuySell.sol
@@ -4,14 +4,15 @@ import "./LockOwnership.sol";
 
 // this is how it will work
 // call 
-//
-//
-//
-//
-//
-//
 
 contract LockBuySell is LockOwnership {
+    /** events */
+    event SellOrderCreated(uint256,uint256,address);
+    event SellOrderCancelled( SellOrder sellorder, uint256 id);
+    event SellOrderFulFilled(uint256,uint256,address,address);
+    
+
+
     struct SellOrder {
         address seller ;
         uint256 sellingPrice;
@@ -19,19 +20,20 @@ contract LockBuySell is LockOwnership {
         uint8 status;
         uint lock_id;
     }
+
     /*** Storage**/
     mapping(uint256=>SellOrder) public tokenIdToSellOrder;
     // mapping to get the no of sell orders per address 
     mapping(address => uint256) sellOrderCount;
+
     function _isOnSale(uint256 _tokenId) internal view returns(bool) {
         return (tokenIdToSellOrder[_tokenId].status == 1);
     }
     function _removeSellOrder(uint256 _tokenId) internal {
+        SellOrderCancelled(tokenIdToSellOrder[_tokenId],_tokenId);
         delete tokenIdToSellOrder[_tokenId];
     }
-    function _approveOwner (uint lock_id) external {
-        this.approve(this,lock_id);
-    }
+    
 
     // fetch the lock , check owner is the msg.sender , change lockstatus to onsale 
     // will be called by owner of lock
@@ -46,8 +48,8 @@ contract LockBuySell is LockOwnership {
 
         //TODO is this needed 
         sellingLock.lockStatus = 2;
-        //issue solved by making it external 
-        _approveOwner(_lock_id);
+
+        _approve(_lock_id,this);
         SellOrder memory _sellorder = SellOrder({
             seller: msg.sender,
             sellingPrice: price,
@@ -58,29 +60,46 @@ contract LockBuySell is LockOwnership {
         tokenIdToSellOrder[_lock_id] = _sellorder;
         //checks overflow
         //emit sell event
+        SellOrderCreated(price,_lock_id,msg.sender);
         
     }
     // checks if the sender is owner of lock , checks if the lock is on sale 
-    // what happens if the token id which is provide is not on sale , i guess it wil recieve status as 0
     function cancelSellOrder(uint256 token_id) {
+        // check if the msg.sender owns the lock
         require(_owns(msg.sender,token_id));
+        //check if the lock is on sale
         require(_isOnSale(token_id));
+        // remove the lock sell order
         _removeSellOrder(token_id);
     }
-    function buySellOrder(uint256 token_id) external payable  {
-        tokenIdToSellOrder[token_id];
+    function buySellOrder(uint256 token_id, uint256 amount ) external payable {
+        // check if the given lock is on sale
         require(_isOnSale(token_id));
 
-        // transfer ownership to caller 
-        // msg.value from line 85 
-        // to be countinued from here
+        // change status of lock to defaut
+        Lock storage sellingLock = locks[token_id];
+        sellingLock.lockStatus = 0;
+        
+        // fetch seller and price before deleting
         address seller_address = tokenIdToSellOrder[token_id].seller;  
         uint256 selling_price = tokenIdToSellOrder[token_id].sellingPrice;
-        require(selling_price <= msg.value);
-        _removeSellOrder(token_id);
-        seller_address.transfer(selling_price);
-        this.transferFrom(seller_address, msg.sender, token_id);
+        require(selling_price <= amount);
 
+        // remove sell order to prevent reentrancy attack
+        _removeSellOrder(token_id);
+
+        // TODO if the  buyer sends more money then needed return back
+        //uint256 remainder = amount-selling_price;
+        
+        // not working 
+        //seller_address.transfer(selling_price);
+        
+        require(_approvedFor(this, token_id));
+        require(_owns(seller_address, token_id)); 
+
+        // Reassign ownership (also clears pending approvals and emits Transfer event).
+        _transfer(seller_address, msg.sender ,token_id);        
+        SellOrderFulFilled(token_id,selling_price,seller_address,msg.sender);
 
     }
     
