f43ce4f46554c6655623545992c1897baab7ab68
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-03-06 19:40:41 +0530

    removed unnecessary interface from License

--- LockLicense/LockLicense.sol
+++ LockLicense/LockLicense.sol
@@ -93,38 +93,9 @@ contract LockAccessControl {
 }
 contract LockBase {
 
-    /*structs*/
-    struct Lock {
-    string lockBlueprint;
-    uint64 creationTime;
-    //parent id array
-    uint256[] parentArray;
-    // we wont be able to store the incoming info directly
-    //mapping (uint256 => uint256) parentArray;
-    // status means onrent, for sale , locked , unlocked and more
-    // for now lets assume , 0 is the default , unlocked,unrented,notonsale
-    // 2 means on sale
-    // 1 means on chain
-    uint256 lockStatus;
-    uint256 lettersLimit;
-    uint256 picsLimit;
-    }
-
-    struct LockedLock {
-    string message;
-    string partner;
-    }
-
-    // this array will store all locks , we give id we get lock object , simple and sweet !
-    Lock[] public locks;
-
     /*Variables*/
-    uint256 public lockedLocksCount;
     uint256 public lastPosition;
-
     /*Mappings*/
-    // this array will contain all the locked locks
-    mapping(uint256 => LockedLock) public lockedLocks;
     // checks position and returns if its filled or not , returns true if filled else false
     mapping (uint256 => bool) public checkIfFilled;
     // connects lockid and locked lock position
@@ -138,31 +109,17 @@ contract LockBase {
 
     /** Setters */
     function ADDlockedLocks(uint256 _lockId, string _message, string _partner) external {}
-    function SETlockIndexToOwner(uint256 _lockId, address _address) external {}
-    function SETownershipTokenCount(address _addr, uint _count) external  {}
-    function SETlockIndexToApproved(uint256 _id,address _addr) external  {}
-    function SETlimitIncreaseToRate(uint256 _id, uint _increment) external {}
-    function SETcheckMultiplierForPosition(uint256 _id,uint _multiplier) external {}
     function SETcheckIfFilled(uint256 _id,bool _boolean) external {}
     function SETtokenIdToLockedLockPosition(uint256 _id, uint _pos) external {}
-    function SETtimeToRateMapping(uint64 _time, uint256 _rate) external {}
     function SETlastPosition(uint256 _pos) external {}
     function incrementLockedLocksCount() external {}
     function decrementLockedLocksCount() external {}
     function incrementLastPosition() external {}
 
     /** Lock Getters */
-    function get_Lock_blueprint(uint256 _id) external view returns (string _blueprint){}
-    function get_Lock_creationTime(uint256 _id) external view returns (uint64 _creationtime){}
-    function get_Lock_parents(uint256 _id) external view returns (uint256[] _parentIds){}
     function GETlockStatus(uint256 _id) external view returns (uint256 _lockStatus){}
-    function GETlockletterLim(uint256 _id) external view returns (uint256 _lettersLimit){}
-    function GETlockpicsLim(uint256 _id) external view returns (uint256 _picslimit){}
-
     /** Lock setters */
     function SETlockstatus(uint256 _id,uint256 lockstatus) external {}
-    function SETlockletterLim(uint256 _id, uint256 _letterLim) external {}
-    function SETlockpicLim(uint256 _id,uint256 _picLim) external {}
 
     /** Lock Removers */
     function REMOVElockedLocks(uint256 _pos) external{}
